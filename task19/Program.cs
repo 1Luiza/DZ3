1ac4779b087a6185761d14715b4fdfb16d8e6919
Луиза Анисимова <monalyiza@mail.ru>
2022-09-28 17:23:56 +0900

    решение задач 19, 21, 23

---
+++ task19/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+void palindrom (int number)
+{
+    int firstDigit = number / 10000;
+    int fifthDigit = number % 10;
+    if (firstDigit == fifthDigit)
+    {
+        number = number / 10;
+        int secondDigit = (number / 100) % 10;
+        int fourthDigit = number % 10;
+        if(secondDigit == fourthDigit)
+        {
+            Console.WriteLine("Да");
+        } 
+    else Console.WriteLine("Нет");        
+    }
+}
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+palindrom(number);
---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] TableOfCubesOfNumbers (int number)
+{
+    int[] result = new int [number];
+    for(int i = 1; i <= number; i++ )
+    {
+        result[i - 1] = (int)(Math.Pow(i, 3));
+    }
+    return result;
+} 
+int[] table = TableOfCubesOfNumbers(number);
+{
+    for(int i = 0; i < table.Length; i++)
+    {
+        Console.Write(table[i]+" ");
+    }
+}
